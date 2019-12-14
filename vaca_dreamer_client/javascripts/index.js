b0e7284d5b64fb4e53f3a5b6ca563c8588682b69
Laura Berge <lauraeberge@gmail.com>
2019-12-14 17:47:20 -0600

    added some basic buttons via js that will eventually load content

--- vaca_dreamer_client/javascripts/index.js
+++ vaca_dreamer_client/javascripts/index.js
@@ -2,5 +2,32 @@
 const prntEl = document.getElementById('parent-el')
 
 document.addEventListener("DOMContentLoaded", (e) => {
-    // add all function calls needed here
+    console.log("DOM loaded");
+    addButtons();
 })
+
+function refreshContent () {
+    prntEl.innerHTML = "";
+}
+
+function addButtons () {
+    let div = document.createElement('div');
+    div.className = 'buttons center';
+    let plan = document.createElement('button');
+    plan.className = "plan";
+    plan.innerHTML = "Get Vaca Inspiration";
+    let create = document.createElement('button');
+    create.className = "create-vaca";
+    create.innerHTML = "Create New Vaca";
+
+    div.appendChild(plan);
+    div.appendChild(create);
+    prntEl.appendChild(div);
+
+    buttonEvent(plan, create);
+}
+
+function buttonEvent(plan, create) {
+    plan.addEventListener("click", (e) => {console.log("PLAN!")});
+    create.addEventListener("click", (e) => {console.log("CREATE!")});
+}
