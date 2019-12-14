38b30cf1e33e1c4f59b647e934331cdbf30491c8
Laura Berge <lauraeberge@gmail.com>
2019-12-14 09:39:16 -0600

    added a bit of html/css/js to get started

---
+++ vaca_dreamer_client/javascripts/index.js
@@ -0,0 +1,6 @@
+// this div is what we'll append all of the js generated html to...
+const prntEl = document.getElementById('parent-el')
+
+document.addEventListener("DOMContentLoaded", (e) => {
+    // add all function calls needed here
+})
